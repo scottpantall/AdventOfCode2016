d9facba0cbceef7e6e6a1439da3ff40976ea29d9
Scott Pantall <aspantall@gmail.com>
2016-12-08 08:02:13 -0700

    Day 3 first try

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,31 @@ namespace Day3
     {
         static void Main(string[] args)
         {
+            string line;
+            List<string> possibleTriangles = new List<string>();
+            int answer;
+            char[] delimiters = { ' ' };
+
+            // Read the file and display it line by line
+            System.IO.StreamReader file = new System.IO.StreamReader("../../input.txt");
+
+            while ((line = file.ReadLine()) != null)
+            {
+                string[] numbers;
+
+                line = line.Trim();
+                numbers = line.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+                
+
+                if(int.Parse(numbers[0]) + int.Parse(numbers[1]) > int.Parse(numbers[2]))
+                {
+                    possibleTriangles.Add(line);
+                }
+            }
+
+            answer = possibleTriangles.Count();
+
+            Console.WriteLine("There are " + answer + " possible triangles.");
         }
     }
 }
